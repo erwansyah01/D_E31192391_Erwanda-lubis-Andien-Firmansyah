4462fc2581525a5b75b9b04cac898a1f743a98a1
erwansyah01 <65637254+erwansyah01@users.noreply.github.com>
2021-03-15 23:28:52 +0700

    next commit

---
+++ Minggu6/kegiatan/kode1/hello.php
@@ -0,0 +1,7 @@
+<?php
+class Hello extends CI_Controller{
+	public function index(){
+		echo "<h2>Hello WOrld CI</h2>";
+	}
+}
+?>
---
+++ Minggu6/kegiatan/kode3/helloview.php
@@ -0,0 +1,12 @@
+<html>
+<!--<head>Controller dan View</head>-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Controller, Model dan view</title>
+</head>
+
+<body>
+	<h2><? echo $teks; ?><h2>
+    <h3>Menggunakan Controller, Model dan View!</h3>
+</body>
+</html>
