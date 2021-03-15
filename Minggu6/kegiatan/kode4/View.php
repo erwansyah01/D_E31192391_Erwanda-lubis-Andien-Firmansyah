74962387d864f3a9cdd481787c91b2e3920fc90e
erwansyah01 <65637254+erwansyah01@users.noreply.github.com>
2021-03-15 23:51:22 +0700

    next commit

---
+++ Minggu6/kegiatan/kode4/View.php
@@ -0,0 +1,14 @@
+<html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Controller dan view lebih dari 1 variabel</title>
+</head>
+
+<body>
+	<!--Memanggil variabel 1-->
+    variabel1 <?php echo variabel1; ?></br>
+    
+    <!--Memanggil variabel 2-->
+    variabel1 <?php echo variabel2; ?></br>
+</body>
+</html>
