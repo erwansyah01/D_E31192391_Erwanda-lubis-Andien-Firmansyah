b52e2853cf2603a2053889fcd8db93a1cc6a4d5a
erwansyah01 <65637254+erwansyah01@users.noreply.github.com>
2021-03-15 23:52:58 +0700

    Delete Controller.php

--- Minggu6/kegiatan/kode3/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Variable extends CI_Controller{
-	public function inedx(){
-		$data = ['variabel1'=>'Data variabel ke 1', 'variabel2'=>'Data variabel ke 2];
-		
-		$this->load->view('variabelview', $data);
-	}
-}
-?>
